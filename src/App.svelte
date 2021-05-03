ca5c2c3e2f35f55ade92e2b0ba15a13ed3a30be8
Geoff Rich <geoff.rich@alaskaair.com>
2021-05-03 08:09:37 -0700

    Add properties action and displayHTML component

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,63 @@
 <script>
-  setTimeout(loadCustomElementDefinition, 500);
+	import DisplayHTML from './DisplayHTML.svelte';
 
-  let names = ['Amy', 'Rory', 'Clara', 'Bill'];
-  let selectedNames = ['Clara'];
-  let counter = 0;
+	setTimeout(() => import('./fancy-greeting'), 500);
 
-  let element;
-  let elementHtml = '';
+	let names = ['Amy', 'Rory', 'Clara', 'Bill'];
+	let selectedNames = ['Clara'];
+	let greeting = 'Hello';
+	let counter = 0;
 
-  function getOuterHtml(node) {
-    elementHtml = node.outerHTML;
-  }
+	function setProperties(node, properties) {
+		const applyProperties = () => {
+			Object.entries(properties).forEach(([k, v]) => {
+				node[k] = v;
+			});
+		};
 
-  function loadCustomElementDefinition() {
-    import('./fancy-greeting');
-  }
+		applyProperties();
+
+		return {
+			update(updatedProperties) {
+				properties = updatedProperties;
+				applyProperties();
+			}
+		};
+	}
 </script>
 
 <main>
-  <h1>Svelte and custom elements: properties vs attributes</h1>
+	<h1>Svelte and custom elements: properties vs attributes</h1>
 
-  <p>
-    On initial render, the custom element definition has not been loaded. This
-    causes Svelte to set everything as attributes using setAttribute. Click the
-    button to re-render the custom element. Since the custom element is now
-    defined, Svelte sets everything as properties.
-  </p>
+	<p>
+		On initial render, the custom element definition has not been loaded. This
+		causes Svelte to set everything as attributes using setAttribute. Click the
+		button to re-render the custom element. Since the custom element is now
+		defined, Svelte sets everything as properties.
+	</p>
 
-  <button on:click={() => counter++}>Re-render custom element</button>
-  <p>Re-rendered {counter} times</p>
+	<button on:click={() => counter++}>Re-render custom element</button>
+	<p>Re-rendered {counter} times</p>
 
-  {#key counter}
-    <fancy-greeting
-      greeting={'Hello'}
-      bind:this={element}
-      names={selectedNames}
-      use:getOuterHtml
-    />
-  {/key}
+	{#key counter}
+		<h2>Default</h2>
+		<DisplayHTML>
+			<fancy-greeting {greeting} names={selectedNames} />
+		</DisplayHTML>
+		<h2>With properties action</h2>
+		<DisplayHTML>
+			<fancy-greeting {greeting} use:setProperties={{ names: selectedNames }} />
+		</DisplayHTML>
+	{/key}
 
-  <pre>{elementHtml}</pre>
-
-  <fieldset>
-    <legend>Choose names</legend>
-    {#each names as name}
-      <label
-        ><input type="checkbox" bind:group={selectedNames} value={name} />
-        {name}</label
-      >
-    {/each}
-  </fieldset>
+	<label>Greeting <input type="text" bind:value={greeting} /></label>
+	<fieldset>
+		<legend>Choose names</legend>
+		{#each names as name}
+			<label
+				><input type="checkbox" bind:group={selectedNames} value={name} />
+				{name}</label
+			>
+		{/each}
+	</fieldset>
 </main>
-
-<style>
-  pre {
-    white-space: pre-wrap;
-  }
-</style>
---
+++ src/DisplayHTML.svelte
@@ -0,0 +1,18 @@
+<script>
+	let elementHTML = '';
+
+	function getHTML(node) {
+		elementHTML = node.innerHTML;
+	}
+</script>
+
+<div use:getHTML>
+	<slot />
+</div>
+<pre>{elementHTML}</pre>
+
+<style>
+	pre {
+		white-space: pre-wrap;
+	}
+</style>
